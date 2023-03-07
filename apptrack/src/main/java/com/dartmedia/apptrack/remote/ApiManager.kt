7a16b73a96405786e7e5825a73cccd68c79e0a5c
NeoDesk <dzalfikriali@gmail.com>
2023-03-07 14:26:33 +0700

    Add ApiManager for set Base Url Dynamically

---
+++ apptrack/src/main/java/com/dartmedia/apptrack/remote/ApiManager.kt
@@ -0,0 +1,24 @@
+package com.dartmedia.apptrack.remote
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class ApiManager(context: Context) {
+    private var preferences: SharedPreferences =
+        context.getSharedPreferences(APP_NAME, Context.MODE_PRIVATE)
+
+    fun setBaseUrl(url: String) {
+        val editor = preferences.edit()
+        editor.putString(USER_BASE_URL, url)
+        editor.apply()
+    }
+
+    fun fetchBaseUrl(): String? {
+        return preferences.getString(USER_BASE_URL, null)
+    }
+
+    companion object {
+        const val USER_BASE_URL = "user_base_url"
+        const val APP_NAME = "tracking_app" //TODO (Change to Core module name)
+    }
+}
